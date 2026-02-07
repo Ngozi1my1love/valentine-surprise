065e8f964e8acb37981f95582df1648423267244
Ngozi1my1love <ghghostrespect@gmail.com>
2026-02-07 15:53:45 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,33 @@ const yesBtn = document.getElementById('yesBtn');
 const noBtn = document.getElementById('noBtn');
 const questionContainer = document.getElementById('questionContainer');
 const successContainer = document.getElementById('successContainer');
+const bgMusic = document.getElementById('bgMusic');
+const successMusic = document.getElementById('successMusic');
+const confettiCanvas = document.getElementById('confetti');
+const ctx = confettiCanvas.getContext('2d');
+
+// Set canvas size
+confettiCanvas.width = window.innerWidth;
+confettiCanvas.height = window.innerHeight;
+
+// Resize canvas on window resize
+window.addEventListener('resize', function() {
+    confettiCanvas.width = window.innerWidth;
+    confettiCanvas.height = window.innerHeight;
+});
+
+// Play background music when page loads
+window.addEventListener('load', function() {
+    bgMusic.play().catch(function(error) {
+        console.log('Autoplay prevented. User interaction needed.');
+    });
+});
+
+// Enable music on first user interaction (for browsers that block autoplay)
+document.addEventListener('click', function enableMusic() {
+    bgMusic.play();
+    document.removeEventListener('click', enableMusic);
+}, { once: true });
 
 // Function to move the "No" button away
 function moveNoButton() {
@@ -30,13 +57,83 @@ noBtn.addEventListener('click', function(e) {
     moveNoButton();
 });
 
+// Confetti particle class
+class Confetti {
+    constructor() {
+        this.x = Math.random() * confettiCanvas.width;
+        this.y = -10;
+        this.size = Math.random() * 8 + 5;
+        this.speedY = Math.random() * 3 + 2;
+        this.speedX = Math.random() * 2 - 1;
+        this.color = this.randomColor();
+        this.rotation = Math.random() * 360;
+        this.rotationSpeed = Math.random() * 10 - 5;
+    }
+    
+    randomColor() {
+        const colors = ['#ff6b6b', '#ee5a6f', '#f06595', '#cc5de8', '#845ef7', '#5c7cfa', '#339af0', '#22b8cf', '#20c997', '#51cf66', '#94d82d', '#ffd43b', '#ff922b'];
+        return colors[Math.floor(Math.random() * colors.length)];
+    }
+    
+    update() {
+        this.y += this.speedY;
+        this.x += this.speedX;
+        this.rotation += this.rotationSpeed;
+        
+        if (this.y > confettiCanvas.height) {
+            this.y = -10;
+            this.x = Math.random() * confettiCanvas.width;
+        }
+    }
+    
+    draw() {
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.rotation * Math.PI / 180);
+        ctx.fillStyle = this.color;
+        ctx.fillRect(-this.size / 2, -this.size / 2, this.size, this.size);
+        ctx.restore();
+    }
+}
+
+let confettiParticles = [];
+let animationId;
+
+function createConfetti() {
+    for (let i = 0; i < 150; i++) {
+        confettiParticles.push(new Confetti());
+    }
+}
+
+function animateConfetti() {
+    ctx.clearRect(0, 0, confettiCanvas.width, confettiCanvas.height);
+    
+    confettiParticles.forEach(particle => {
+        particle.update();
+        particle.draw();
+    });
+    
+    animationId = requestAnimationFrame(animateConfetti);
+}
+
 // When "Yes" is clicked
 yesBtn.addEventListener('click', function() {
+    // Stop background music
+    bgMusic.pause();
+    bgMusic.currentTime = 0;
+    
+    // Play success music
+    successMusic.play();
+    
     // Hide the question
     questionContainer.classList.add('hidden');
     
     // Show the success message with photo
     successContainer.classList.remove('hidden');
+    
+    // Start confetti
+    createConfetti();
+    animateConfetti();
     
     // Send email notification
     sendEmailNotification();
@@ -44,14 +141,13 @@ yesBtn.addEventListener('click', function() {
 
 // Function to send email notification
 function sendEmailNotification() {
-    // We'll use Formspree for this - you'll add your email endpoint here
     fetch('https://formspree.io/f/mgoleyln', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            message: 'Ngozi said YES! 💕',
+            message: 'She said YES! 💕',
             timestamp: new Date().toLocaleString()
         })
     }).then(response => {
